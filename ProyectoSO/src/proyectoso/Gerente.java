92b1858bc14c0f0196684cbf395812bd2918afe9
Oriana Gonzalez <go.oriana@correo.unimet.edu.ve>
2021-02-24 18:46:10 -0400

    tiempos arreglados y text fields bloqueados

--- ProyectoSO/src/proyectoso/Gerente.java
+++ ProyectoSO/src/proyectoso/Gerente.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
  */
 public class Gerente extends Thread {
     Semaphore mutexTiempo, mutexConsolas;
-    double tiempoDormir;
+    int tiempoDormir;
 
     public Gerente(Semaphore mutexTiempo, Semaphore mutexConsolas) {
         this.mutexTiempo = mutexTiempo;
@@ -26,14 +26,20 @@ public class Gerente extends Thread {
     public void run() {
         while (true) {
             try {
+                
                 Central.accionGerente = "Esperando";
                 ProyectoSO.dashboard.setAccionGerente(Central.accionGerente);
+                System.out.println("soy gerente y estoy esperando" + Central.accionGerente);
                 this.mutexTiempo.acquire();
                     Central.accionGerente = "Consultando";
+                    
+                    System.out.println("SOY GERENTE Y ESTOY CONSULTANDO" + Central.accionGerente);
                     ProyectoSO.dashboard.setAccionGerente(Central.accionGerente);
                     if (Central.diasRestantes == 0) {
+                        
                         this.mutexConsolas.acquire();
                             //Despliegue y se reinicializan contadores
+                            
                             Central.accionGerente = "Desplegando";
                             ProyectoSO.dashboard.setAccionGerente(Central.accionGerente);
                             Central.consolasProducidas = 0;
@@ -44,7 +50,8 @@ public class Gerente extends Thread {
                 this.mutexTiempo.release();
                 Central.accionGerente = "Durmiendo";
                 ProyectoSO.dashboard.setAccionGerente(Central.accionGerente);
-                Thread.sleep((long) this.tiempoDormir);    
+                System.out.println("soy gerente y me voy a dormir");
+                Thread.sleep(this.tiempoDormir);    
                 
             } catch (InterruptedException ex) {
                 System.out.println("Fallo en el Geretnte "+ex);
